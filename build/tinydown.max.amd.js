4014a6addcfd9f19b66074c2dc2fdc32a34e1db8
Andrea Giammarchi <agiammarchi@twitter.com>
2013-04-12 21:48:04 -0700

    all tests are green

--- build/tinydown.max.amd.js
+++ build/tinydown.max.amd.js
@@ -31,14 +31,18 @@ define(function(){
     LIST = "",
     c0 = "\x00",
     c1 = "\x01",
+    c2 = "\x02",
+    c3 = "<blockquote/>",
     // some common RegExp shortcut
     re0 = /\x00/g,
     re1 = /\x01/g,
+    re2 = /\x02([^\x00]*?)\x02/g,
+    re3 = /<blockquote\/>/g,
     N = "\n",
     LF = "\\r\\n|\\r|\\n",
     SL = "(?:^|" + LF + ")",
     NL = "(?:$|" + LF + ")",
-    NOT_AFTER = "(?:" + LF + "(?!\\1)|$)",
+    NOT_AFTER = "(?=" + LF + "(?!\\1)|$)",
     ALL = "([^\\x00]*?)",
     LINK = "\\[(.*?)\\]\\((.+?)(?: (&(?:quot|apos);)?(.+?)(\\3))?\\)",
     ANYTHING = "(.+?)" + NL,
@@ -52,9 +56,13 @@ define(function(){
       "&gt; ":   /^&gt; /gm
     },
     header = /^#+$/,
+    trim = /^\s+|\s+$/g,
     num = /^\d/,
     s = /  +/g,
     sl = new RegExp("(?:^" + LF + ")", GM),
+    strim = ''.trim || function () {
+      return this.replace(trim, '');
+    },
     // find strings
     find = [
       "&(?!#?[a-z0-9]+;)", G,
@@ -70,10 +78,11 @@ define(function(){
       SL + "(\\t| {4})" + ALL + NOT_AFTER, G,
       "(`{1,2})([^\\r\\n]+?)\\1", GM,
       "^(#{1,6})\\s+" + ANYTHING, GM,
-      "  +" + NL, G,
+      "  +" + N, G,
       "([_*]{1,2})([^\\2]+?)(\\1)", G,
       "!?" + LINK, GM,
-      SL + "(&gt; )" + ALL + NOT_AFTER, G
+      SL + "(&gt; )" + ALL + NOT_AFTER, G,
+      SL + "(?!<)([^\\x00]*?)(?=" + NL + "{2,})", G
     ],
     // the list of RegExp objects
     re = [],
@@ -87,12 +96,12 @@ define(function(){
       null,
       "<h1>$1</h1>",
       "<h2>$1</h2>",
-      "<hr/>" + N,
+      "<hr/>",
       function(m, $1, $2, $3, $4, $5) {
-        tmp = "<li>" + tinydown($3.replace(s, "")).replace(sl, "<br/>" + N) + "</li>" + N;
+        tmp = "<li>" + tinydown($3.replace(s, "")).replace(sl, "<br/>") + "</li>";
         if ($4 === 0) {
           LIST = num.test($2) ? '<ol>' : '<ul>';
-          tmp = LIST + N + tmp;
+          tmp = LIST + tmp;
         } else if ($5.length - ($4 + m.length) < 2) {
           tmp += LIST.replace("<", "</");
         }
@@ -103,7 +112,7 @@ define(function(){
       function (m, c, s, t) {
         return "<h" + (t = c.length) + ">" + s.replace(header, "") + "</h" + t + ">";
       },
-      "<br/>" + N,
+      "<br/>",
       function (m, c, s, t) {
         return "<" + (t = c.length == 2 ? "strong>" : "em>") + s + "</" + t;
       },
@@ -113,23 +122,34 @@ define(function(){
           '<a href="' + $2 + '" title="' + $4 + '">' + $1 + '</a>'
         ;
       },
-      function(m, $1, $2) {
-        return "<" + BLOCKQUOTE + tinydown($2.replace(commonBlocks[$1], '')) + "</" + BLOCKQUOTE;
-      }
+      null,
+      c2 + "$1" + c2
     ],
     tmp,
     i = 0; i < find.length; re.push(new RegExp(find[i++], find[i++]))
   );
+  function paragraphs(m, $1) {
+    $1 = strim.call($1);
+    return $1.length ? ($1.charAt(0) === "<" ? $1 : "<p>" + $1 + "</p>") : "";
+  }
   function tinydown(string) {
     for (var
       zero = [],
       one = [],
+      bquote = [],
       special = {
         "5": function (m, $1, $2) {
           return ($1.length ? zero.push(c0, $1, $2 + N) : zero.push($2 + N)) && c0;
         },
         "10": function (m, $1, $2) {
           return one.push($2.replace(commonBlocks[$1], '') + N) && c1;
+        },
+        "16": function(m, $1, $2) {
+          bquote.push(strim.call($2.charAt(0) + $2.slice(1).replace(commonBlocks[$1], '')));
+          return c3;
+          return bquote.push("<" + BLOCKQUOTE + tinydown(
+            strim.call($2.charAt(0) + $2.slice(1).replace(commonBlocks[$1], ''))
+          ) + "</" + BLOCKQUOTE) && c3;
         }
       },
       i = 0; i < re.length; i++
@@ -140,14 +160,18 @@ define(function(){
     return string.
       replace(re0, function(s){
         s = zero.shift();
-        return N + (
+        return (
           s === c0 ?
             PRE_CODE + ' class="' + zero.shift() + '">' + zero.shift() :
             PRE_CODE + ">" + s
-          ) + CODE_PRE + N;
+          ) + CODE_PRE;
         }).
         replace(re1, function(s){
-          return N + PRE_CODE + ">" + one.shift() + CODE_PRE + N;
+          return PRE_CODE + ">" + one.shift() + CODE_PRE;
+        }).
+        replace(re2, paragraphs).
+        replace(re3, function () {
+          return "<" + BLOCKQUOTE + tinydown(bquote.shift()) + "</" + BLOCKQUOTE;
         })
       ;
   }
