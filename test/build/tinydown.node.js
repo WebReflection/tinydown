ba9ea37759a9a778f87d08cc328b4c84e49b2136
Andrea Giammarchi <agiammarchi@twitter.com>
2013-04-12 22:13:55 -0700

    automatic test generator

--- test/build/tinydown.node.js
+++ test/build/tinydown.node.js
@@ -20,76 +20,157 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 */
-
-/**
- * Just a tiny markdown like parser.
- * @example tinydown(node.textContent || textarea.value || genericString)
- * @param   {string} input the generic text to parse via markdown
- * @return  {string} the input as html
- */
-function tinydown(input) {"use strict";
+var tinydown = function(){
   /* jshint loopfunc: true */
   for(var
-    // some common RegExp shortcut
+    // constants
+    PRE_CODE = "<pre><code",
     CODE_PRE = "</code></pre>",
     BLOCKQUOTE = "blockquote>",
-    NL = "(?:\\r\\n|\\r|\\n|$)",
-    LINK = "\\[(.*?)\\]\\((.+?)(?: (&(?:quot|apos);)(.+?)(\\3))?\\)",
+    TRIPLETICK = "```",
+    LIST = "",
+    c0 = "\x00",
+    c1 = "\x01",
+    c2 = "\x02",
+    c3 = "<blockquote/>",
+    // some common RegExp shortcut
+    re0 = /\x00/g,
+    re1 = /\x01/g,
+    re2 = /\x02([^\x00]*?)\x02/g,
+    re3 = /<blockquote\/>/g,
+    N = "\n",
+    LF = "\\r\\n|\\r|\\n",
+    SL = "(?:^|" + LF + ")",
+    NL = "(?:$|" + LF + ")",
+    NOT_AFTER = "(?=" + LF + "(?!\\1)|$)",
+    ALL = "([^\\x00]*?)",
+    LINK = "\\[(.*?)\\]\\((.+?)(?: (&(?:quot|apos);)?(.+?)(\\3))?\\)",
     ANYTHING = "(.+?)" + NL,
-    // temporary container for code blocks ... these won't be parsed
-    L = [],
-    // list of search, replacement for the given input
-    re = [
-      // make HTML safe
-      "&(?!#?[a-z0-9]+;)", "&amp;",
-      "<", "&lt;",
-      ">", "&gt;",
-      '"', "&quot;",
-      "'", "&apos;",
-      // drop code blocks from the input, no parsing required
-      "^(?:\\t| {4})" + ANYTHING, function(m, s){return L.push(s + "\n") && "\0";},
-      // parse H1 and H2
-      "^" + ANYTHING + "=+" + NL, "<h1>$1</h1>\n",
-      "^" + ANYTHING + "-+" + NL, "<h2>$1</h2>\n",
-      // parse H1, H2, H3, H4, H5, H6 with other notation
-      "^(#+)\\s*" + ANYTHING, function(m, c, s, t){
-        return "<h" + (t = c.length) + ">" + s.replace(/#+$/, "") + "</h" + t + ">\n";
+    // flags
+    G = "g",
+    GM = G + "m",
+    // reused common blocks regexp
+    commonBlocks = {
+      "\t":   /^\t/gm,
+      "    ": /^    /gm,
+      "&gt; ":   /^&gt; /gm
+    },
+    header = /^#+$/,
+    trim = /^\s+|\s+$/g,
+    num = /^\d/,
+    s = /  +/g,
+    sl = new RegExp("(?:^" + LF + ")", GM),
+    strim = ''.trim || function () {
+      return this.replace(trim, '');
+    },
+    // find strings
+    find = [
+      "&(?!#?[a-z0-9]+;)", G,
+      "<", G,
+      ">", G,
+      '"', G,
+      "'", G,
+      SL + TRIPLETICK + "([a-zA-Z]*)(?:" + LF + ")" + ALL + "(?:" + LF + ")" + TRIPLETICK + NL, G,
+      "^" + ANYTHING + "=+" + NL, GM,
+      "^" + ANYTHING + "-+" + NL, GM,
+      "^(?:\\* \\* |- - |\\*\\*|--)[-*][-* ]*" + NL, GM,
+      SL + "( *)(\\* |\\+ |- |\\d+. )" + ALL + "(?=" + LF + "(?!\\1  )|$)", G,
+      SL + "(\\t| {4})" + ALL + NOT_AFTER, G,
+      "(`{1,2})([^\\r\\n]+?)\\1", GM,
+      "^(#{1,6})\\s+" + ANYTHING, GM,
+      "  +" + N, G,
+      "([_*]{1,2})([^\\2]+?)(\\1)", G,
+      "!?" + LINK, GM,
+      SL + "(&gt; )" + ALL + NOT_AFTER, G,
+      SL + "(?!<)([^\\x00]*?)(?=" + NL + "{2,})", G
+    ],
+    // the list of RegExp objects
+    re = [],
+    // what to replace
+    place = [
+      "&amp;",
+      "&lt;",
+      "&gt;",
+      "&quot;",
+      "&apos;",
+      null,
+      "<h1>$1</h1>",
+      "<h2>$1</h2>",
+      "<hr/>",
+      function(m, $1, $2, $3, $4, $5) {
+        tmp = "<li>" + tinydown($3.replace(s, "")).replace(sl, "<br/>") + "</li>";
+        if ($4 === 0) {
+          LIST = num.test($2) ? '<ol>' : '<ul>';
+          tmp = LIST + tmp;
+        } else if ($5.length - ($4 + m.length) < 2) {
+          tmp += LIST.replace("<", "</");
+        }
+        return tmp;
       },
-      // parse HR lines
-      "(?:\\* \\* |- - |\\*\\*|--)[-*][-* ]*" + NL, "<hr/>\n",
-      // parse manual br
-      "  +" + NL, "<br/>",
-      // parse ordered and unordered lists
-      "^ *(\\* |\\+ |- |\\d+. )" + ANYTHING, function(m, c, s, t){
-        return "<" + (t = /^\d/.test(c) ? "ol>" : "ul>") + "<li>" + tinydown(s) + "</li></" + t;
+      null,
+      "<code>$2</code>",
+      function (m, c, s, t) {
+        return "<h" + (t = c.length) + ">" + s.replace(header, "") + "</h" + t + ">";
       },
-      // remove superflous parsing
-      "</(ul|ol)>\\s*<\\1>", "",
-      // parse strong and em
-      "([_*]{1,2})([^\\2]+?)(\\1)", function(m, c, s, t){
+      "<br/>",
+      function (m, c, s, t) {
         return "<" + (t = c.length == 2 ? "strong>" : "em>") + s + "</" + t;
       },
-      // parse images
-      "!" + LINK, '<img src="$2" alt="$1" title="$4"/>',
-      // parse links
-      LINK, '<a href="$2" title="$4">$1</a>',
-      // parse blockquotes
-      "^&gt; " + ANYTHING, function(m, s){
-        return "<" + BLOCKQUOTE + tinydown(s)+ "</" + BLOCKQUOTE;
+      function (m, $1, $2, $3, $4) {
+        return m.charAt(0) === '!' ?
+          '<img src="' + $2 + '" alt="' + $1 + '" title="' + $4 + '"/>' :
+          '<a href="' + $2 + '" title="' + $4 + '">' + $1 + '</a>'
+        ;
       },
-      // remove superflous parsing
-      "</" + BLOCKQUOTE + "\\s*<" + BLOCKQUOTE, "",
-      // parse backtick for inline code
-      "(`{1,2})([^\\r\\n]+?)\\1", "<code>$2</code>",
-      // put back blocks of code
-      "\\0", function(s){
-        return "<pre><code>" + L.shift() + CODE_PRE;
-      },
-      // clean up superflous parsing
-      CODE_PRE + "\\s*<pre><code>", ""
+      null,
+      c2 + "$1" + c2
     ],
-    i = 0; i < re.length;
-  ) input = input.replace(new RegExp(re[i++], "gm"), re[i++]);
-  return input;
-}
+    tmp,
+    i = 0; i < find.length; re.push(new RegExp(find[i++], find[i++]))
+  );
+  function paragraphs(m, $1) {
+    $1 = strim.call($1);
+    return $1.length ? ($1.charAt(0) === "<" ? $1 : "<p>" + $1 + "</p>") : "";
+  }
+  function tinydown(string) {
+    for (var
+      zero = [],
+      one = [],
+      bquote = [],
+      special = {
+        "5": function (m, $1, $2) {
+          return ($1.length ? zero.push(c0, $1, $2 + N) : zero.push($2 + N)) && c0;
+        },
+        "10": function (m, $1, $2) {
+          return one.push($2.replace(commonBlocks[$1], '') + N) && c1;
+        },
+        "16": function(m, $1, $2) {
+          return bquote.push(strim.call($2.charAt(0) + $2.slice(1).replace(commonBlocks[$1], ''))) && c3;
+        }
+      },
+      i = 0; i < re.length; i++
+    ) {
+      //console.log(re[i], string, place[i]);
+      string = string.replace(re[i], place[i] || special[i]);
+    }
+    return string.
+      replace(re0, function(s){
+        s = zero.shift();
+        return (
+          s === c0 ?
+            PRE_CODE + ' class="' + zero.shift() + '">' + zero.shift() :
+            PRE_CODE + ">" + s
+          ) + CODE_PRE;
+        }).
+        replace(re1, function(s){
+          return PRE_CODE + ">" + one.shift() + CODE_PRE;
+        }).
+        replace(re2, paragraphs).
+        replace(re3, function () {
+          return "<" + BLOCKQUOTE + tinydown(bquote.shift()) + "</" + BLOCKQUOTE;
+        })
+      ;
+  }
+  return tinydown;
+}();
 module.exports = tinydown.tinydown = tinydown;
--- test/test/tinydown.js
+++ test/test/tinydown.js
@@ -4,9 +4,49 @@ var tinydown = require('../build/tinydown.node.js');
 
 wru.test([
   {
-    name: "main",
+    name: "input/output",
     test: function () {
       wru.assert(typeof tinydown == "function");
+      for (var test = [
+        '-h2', 'hello\n---\n', '<h2>hello</h2>',
+        '- hr', '- - -', '<hr/>',
+        '* hr', '* * *', '<hr/>',
+        'escape HTML', '<>"\'&', '<p>&lt;&gt;&quot;&apos;&amp;</p>',
+        'do not double escape', '&lt;&gt;&quot;&apos;&amp;', '<p>&lt;&gt;&quot;&apos;&amp;</p>',
+        'leave entities intact', '&#039;', '<p>&#039;</p>',
+        'block code tick', '\n```\nalert(123);\n```\n', '<pre><code>alert(123);\n</code></pre>',
+        'block code tick with language', '\n```javascript\nalert(123);\n```\n', '<pre><code class="javascript">alert(123);\n</code></pre>',
+        'inline block code', '`hello there`', '<code>hello there</code>',
+        'inline block code with tick', '``hello ` there``', '<code>hello ` there</code>',
+        'just tick', '`````', '<code>`</code>',
+        'block code spaces', '\n    alert(123);\n', '<pre><code>alert(123);\n</code></pre>',
+        'block code tab', '\n\talert(123);\n\t\talert(456);\n', '<pre><code>alert(123);\n\talert(456);\n</code></pre>',
+        'many blocks', '\n\ta\n.\n```\nb\n```', '<pre><code>a\n</code></pre>\n.<pre><code>b\n</code></pre>',
+        '=h1', 'hello\n===\n', '<h1>hello</h1>',
+        '# h1', '# hello\n', '<h1>hello</h1>',
+        '# h2', '## hello\n', '<h2>hello</h2>',
+        '# h3', '### hello\n', '<h3>hello</h3>',
+        '# h4', '#### hello\n', '<h4>hello</h4>',
+        '# h5', '##### hello\n', '<h5>hello</h5>',
+        '# h6', '###### hello\n', '<h6>hello</h6>',
+        'no h7', '####### hello\n', '<p>####### hello</p>',
+        '-- hr', '---', '<hr/>',
+        '** hr', '***', '<hr/>',
+        'ol', ' 1. a\n    b\n 2. c\n', '<ol><li><p>a<br/>b</p></li><li><p>c</p></li></ol>',
+        'strong', 'this **is** strong', '<p>this <strong>is</strong> strong</p>',
+        'em', 'this _is_ em', '<p>this <em>is</em> em</p>',
+        'em and strong', 'this *is* em __and strong__', '<p>this <em>is</em> em <strong>and strong</strong></p>',
+        'image quoted', '![$alt]($src "$title")', '<img src="$src" alt="$alt" title="$title"/>',
+        'image', '![$alt]($src $title)', '<img src="$src" alt="$alt" title="$title"/>',
+        'link quoted', '[$text]($href "$title")', '<a href="$href" title="$title">$text</a>',
+        'link', '[$text]($href $title)', '<a href="$href" title="$title">$text</a>',
+        'blockquote', '> here a blockuote', '<blockquote><p>here a blockuote</p></blockquote>',
+        // TODO: fix this .. maybe ... 
+        'nested blockquote', '> here a blockuote\n> > with a block quote', '<blockquote><p>here a blockuote<blockquote><p>with a block quote</p></blockquote></p></blockquote>'
+      ], i = 0; i < test.length; i += 3) {
+        wru.log(tinydown(test[i + 1]));
+        wru.assert(test[i], tinydown(test[i + 1]) === test[i + 2]);
+      }
     }
   }
 ]);
